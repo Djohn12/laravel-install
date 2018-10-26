690ee1e26ee757ef4f81e68e0bf0d01babef5c46
xLeDocteurx <lenoirc.fr@gmail.com>
2018-10-26 17:14:50 +0200

    pulling the strings

--- app/Board.php
+++ app/Board.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Board extends Model
 {
-    //
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    
+    public function favorite() 
+    {
+        return $this->belongsTo('App\Favorite');
+    }
+
+    public function categories()
+    {
+        return $this->hasMany('App\Category');
+    }
 }
--- app/SubCatRelation.php
+++ app/SubCatRelation.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubCatRelation extends Model
 {
-    //
+    
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+    
+    public function sub_category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
