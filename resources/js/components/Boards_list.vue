faa79c3579bb1ab5ef447568a58e147e296bde9c
Edmond de Tournadre <edmond92@gmail.com>
2018-10-27 23:18:51 +0200

    display new created board in the boards_list

--- resources/js/components/Boards_list.vue
+++ resources/js/components/Boards_list.vue
@@ -1,15 +1,28 @@
 <template>
 	<div>
-		<!-- if some boards are retrieved with created(), boards variable will be set to true -->
-		<div v-if="boards">
-			<h2>Available boards :</h2>
-			<b-tabs>
-				<b-tab v-for="board in boards_list" :key="board.id" :title="board.name"></b-tab>
-			</b-tabs>
-		</div>
-		<!-- else, it means that no boards were retrieved from the database -->
-		<div v-else>
-			<h2>No board available yet</h2>
+		<div>
+	      <b-button variant="outline-primary" @click="show_board_panel">New Board</b-button>
+	    </div>
+	    <div v-if="board_panel">
+	      <b-input-group class="col-5 mx-auto">
+	        <b-form-input v-model="board_name" type="text" class="form-control" placeholder="Enter your new board name"></b-form-input>
+	        <b-input-group-append>
+	          <b-button variant="outline-primary" @click="add_board()">Add new board</b-button>
+	        </b-input-group-append>
+	      </b-input-group>
+	    </div>
+		<div>
+			<!-- if some boards are retrieved with created(), boards variable will be set to true -->
+			<div v-if="boards">
+				<h2>Available boards :</h2>
+				<b-tabs>
+					<b-tab v-for="board in boards_list" :key="board.id" :title="board.name"></b-tab>
+				</b-tabs>
+			</div>
+			<!-- else, it means that no boards were retrieved from the database -->
+			<div v-else>
+				<h2>No board available yet</h2>
+			</div>
 		</div>
 	</div>
 </template>
@@ -23,14 +36,26 @@
 		},
 		data() {
 			return {
+				board_panel: false,
+      			board_name: '',
 				boards: false,
-				boards_list: {}
+				boards_list: []
 			}
 		},
 		methods: {
-			log() {
-				console.log(this.boards_list);
-			}
+			show_board_panel(){
+      			return this.board_panel = !this.board_panel
+    		},
+			add_board() {
+      			let name = this.board_name;
+      			let data = {
+        			'name': name,
+        			'user_id': currentUser 
+      			}
+      			// ajax call to the api route
+      			window.axios.post('api/boards/store', {data})
+      			.then(response => this.boards_list.push(response.data.new_board));
+    		}
 		},
 		// fetch existing boards with ajax
 		created() {
